b8ca8549243bcc9fdafe8d3caa003f012b0ca1b9
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2023-01-18 21:59:33 -0300

    mimic: deploy swap connector v5 to L2s

---
+++ tasks/2023010605-swap-connector-v5/test/deployed/task.optimism.ts
@@ -0,0 +1,13 @@
+import { getForkedNetwork } from '@mimic-fi/v2-helpers'
+import hre from 'hardhat'
+
+import Task from '../../../../src/task'
+import { itDeploysSwapConnectorCorrectly } from '../behavior'
+
+describe('SwapConnector v5 - optimism', function () {
+  before('load task', async function () {
+    this.task = Task.forTest('2023010605-swap-connector-v5', getForkedNetwork(hre), getForkedNetwork(hre))
+  })
+
+  itDeploysSwapConnectorCorrectly()
+})
