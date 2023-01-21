a7904fe28c4050fc0aec407f198f6d2a49b977f5
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2023-01-21 17:32:02 -0300

    dxdao: re-deploy goerli and mumbai

--- tasks/2023010704-dxdao-bridger-l2/test/deployed/task.mumbai.ts
+++ tasks/2023010704-dxdao-bridger-l2/test/deployed/task.mumbai.ts
@@ -4,7 +4,7 @@ import hre from 'hardhat'
 import Task from '../../../../src/task'
 import { itDeploysDxDaoBridgerCorrectly } from '../behavior.mumbai'
 
-describe.skip('L2 DXdao bridger v1 - mumbai', function () {
+describe('L2 DXdao bridger v1 - mumbai', function () {
   before('load task', async function () {
     this.task = Task.forTest('2023010704-dxdao-bridger-l2', getForkedNetwork(hre), getForkedNetwork(hre))
   })
