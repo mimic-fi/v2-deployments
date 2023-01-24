b2f66eb66ee65b97aa1fccecf889eb68ed4a771a
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2023-01-24 16:07:34 -0300

    chore: test constants

--- constants/chainlink/mumbai.ts
+++ constants/chainlink/mumbai.ts
@@ -1,3 +1,3 @@
 /* eslint-disable no-secrets/no-secrets */
 
-export const MATIC_USD = '0x1ECC4534D0296F7C35971534B3Ea2b6D5DDc2E26' // Mock price feed
+export const MATIC_USD = '0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada'
