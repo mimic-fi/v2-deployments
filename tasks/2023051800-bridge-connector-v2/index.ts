d4309be3bf6fb171f7ebe92c2fceb370efd51473
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2023-05-18 18:34:07 -0300

    Core: Bridge connector V2 (#34)

---
+++ tasks/2023051800-bridge-connector-v2/index.ts
@@ -0,0 +1,16 @@
+import { getSigner } from '@mimic-fi/v2-helpers'
+
+import { deployAndRegisterImplementation } from '../../src/registry'
+import Task from '../../src/task'
+import { TaskRunOptions } from '../../src/types'
+import { BridgeConnectorDeployment } from './input'
+
+export default async (task: Task, { force, from }: TaskRunOptions = {}): Promise<void> => {
+  const input = task.input() as BridgeConnectorDeployment
+  const { namespace, version, wrappedNativeToken, axelarGateway, connext, Registry } = input
+
+  if (!from) from = await getSigner(input.from)
+  const create3Params = { namespace, version, from, force }
+  const args = [wrappedNativeToken, axelarGateway, connext, Registry]
+  await deployAndRegisterImplementation(task, 'BridgeConnector', args, create3Params)
+}
