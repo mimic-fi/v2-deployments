2836d231e2e3d833d4bc94ef4a673bf90ae7be9f
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2023-06-29 21:07:41 -0300

    paraswap: update bridge connector

---
+++ tasks/2023062901-paraswap-update-bridge-connector/input.ts
@@ -0,0 +1,35 @@
+import { TESTING_EOA } from '../../constants/mimic'
+import Task from '../../src/task'
+
+const BridgeConnector = new Task('2023062900-bridge-connector-v5')
+const ParaswapFeeRedistributor = new Task('2023020101-paraswap-fee-redistributor')
+
+export default {
+  owner: TESTING_EOA,
+  SmartVault: ParaswapFeeRedistributor,
+  BridgeConnector,
+  mainnet: {
+    PermissionsManager: new Task('2023062100-paraswap-fee-redistributor-updates-mainnet'),
+  },
+  polygon: {
+    PermissionsManager: new Task('2023062101-paraswap-fee-redistributor-updates-polygon'),
+  },
+  arbitrum: {
+    PermissionsManager: new Task('2023062102-paraswap-fee-redistributor-updates-arbitrum'),
+  },
+  optimism: {
+    PermissionsManager: new Task('2023062103-paraswap-fee-redistributor-updates-optimism'),
+  },
+  bsc: {
+    PermissionsManager: new Task('2023062104-paraswap-fee-redistributor-updates-bsc'),
+  },
+  avalanche: {
+    PermissionsManager: new Task('2023062106-paraswap-fee-redistributor-updates-avalanche'),
+  },
+}
+
+export type ParaswapFeeRedistributorUpdateBridgeConnector = {
+  owner: string
+  SmartVault: string
+  BridgeConnector: string
+}
