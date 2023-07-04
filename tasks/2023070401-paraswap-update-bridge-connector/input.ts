93e8171e3ae4102b4dcd990c9b10eddecc0f60ce
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2023-07-04 09:50:30 -0300

    Paraswap: Update wormhole bridge connector (#44)

---
+++ tasks/2023070401-paraswap-update-bridge-connector/input.ts
@@ -0,0 +1,23 @@
+import { TESTING_EOA } from '../../constants/mimic'
+import Task from '../../src/task'
+
+const BridgeConnector = new Task('2023070400-bridge-connector-v6')
+const ParaswapFeeRedistributor = new Task('2023020101-paraswap-fee-redistributor')
+
+export default {
+  owner: TESTING_EOA,
+  SmartVault: ParaswapFeeRedistributor,
+  BridgeConnector,
+  mainnet: {
+    PermissionsManager: new Task('2023062100-paraswap-fee-redistributor-updates-mainnet'),
+  },
+  avalanche: {
+    PermissionsManager: new Task('2023062106-paraswap-fee-redistributor-updates-avalanche'),
+  },
+}
+
+export type ParaswapFeeRedistributorUpdateBridgeConnector = {
+  owner: string
+  SmartVault: string
+  BridgeConnector: string
+}
